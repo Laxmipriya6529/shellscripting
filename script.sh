db590abf5d8d27dfaf727fc9e477b34bbf6f5ef4
Laxmipriya <laxmipriya6529@gmail.com>
2025-03-22 06:13:47 +0000

    Commiting from first branch

--- script.sh
+++ script.sh
@@ -34,4 +34,39 @@ do
                 break
         fi
 done
+while true
+do
+        read -p "Enter the main directory name: " main1
+        if [ -z $main1 ]
+        then
+                echo "Directory name cannot be empty"
+                exit 1
+        elif [ -e $main1 ]
+        then
+                echo "Directory already exists"
+                read -p "Give a valid directory name : " main1
+        else
+                mkdir $main1
+                break
+        fi
+done
+for i in {1..2}
+do
+	while true
+	do
+        	read -p "Enter the file name: " file1
+        	if [ -z $file1 ]
+        	then
+                	echo "File name cannot be empty"
+                	exit 1
+        	elif [ -e $main1/$file1 ]
+        	then
+                	echo "file already exists"
+                	read -p "Give a valid file name : " file1
+        	else
+                	touch $main1/$file1
+                	break
+        	fi
+	done
+done	
 echo"----------Task Done----------------"
